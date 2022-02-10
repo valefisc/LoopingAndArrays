da6704620ee37dae84928e408048d66fbb0347ae
Valerie <valerie@valerie-fischer.de>
2022-02-11 00:23:53 +0100

    final commit

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-for(i = 1000; i >= 0; i - 2){
+for(let i = 1000; i >= 0; i-=2){
     console.log(i);
 }
-for(i = 0; i <= 10000; i++){
-    if (i = 2500){
+
+for(let i = 0; i <= 10000; i++){
+    if (i === 2500){
         alert ("A quarter of the way there!");
-    } else if (i =5000){
+    } else if (i === 5000){
         alert ("Halfway there!");
-    } else if (i=10000){
+    } else if (i === 10000){
         alert ("The loop is done!");
     }
 }
-let favoriteShows = ["Gilmore Girls", "The 100", "Friends", "How i met your mother", "Denver Clan"];
-for (i = 0; i < favoriteShows.length; i++){
-    console.log("My '" + i + " favorite tv show is " + favoriteShows[i]);
+
+const favoriteShows = ["Gilmore Girls", "The 100", "Friends", "How I met your mother", "Denver Clan"];
+for (let i = 0; i < favoriteShows.length; i++){
+    console.log("My #" + (i+1) + " favorite tv show is " + favoriteShows[i]);
 }
