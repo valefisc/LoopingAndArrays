d1043b59dd10b52c68a5578d5c8ae9f289cd785b
Valerie <valerie@valerie-fischer.de>
2022-02-10 20:12:47 +0100

    did the assignment

---
+++ script.js
@@ -0,0 +1,16 @@
+for(i = 1000; i >= 0; i - 2){
+    console.log(i);
+}
+for(i = 0; i <= 10000; i++){
+    if (i = 2500){
+        alert ("A quarter of the way there!");
+    } else if (i =5000){
+        alert ("Halfway there!");
+    } else if (i=10000){
+        alert ("The loop is done!");
+    }
+}
+let favoriteShows = ["Gilmore Girls", "The 100", "Friends", "How i met your mother", "Denver Clan"];
+for (i = 0; i < favoriteShows.length; i++){
+    console.log("My '" + i + " favorite tv show is " + favoriteShows[i]);
+}
